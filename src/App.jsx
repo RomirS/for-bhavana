371ac1c87c76f75e5fddc5f83e797b0494c88214
Romir Singla <romir@vipan.com>
2023-01-03 14:21:31 -0600

    setup login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Slides from "./pages/Slides";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="" exact element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="slides" element={<Slides />}>
+          <Route path=":s" element={<Slides />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['IBM Plex Mono']
+      },
+    },
+  },
+  plugins: [],
+}
