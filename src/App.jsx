7097f9383db072ae2d83541f0804accf0a021636
Romir Singla <romir@vipan.com>
2023-01-06 12:53:44 -0600

    routes bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Slides from "./pages/Slides";
-import Sli from "./pages/Slide";
+import Slide from "./pages/Slide";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="" exact element={<Login />} />
-        <Route path="login" element={<Login />} />
-        <Route path="slides" element={<Slides />} />
-        <Route path="slides/:s" element={<Sli />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/slides" element={<Slides />} />
+        <Route path="/slides/:s" element={<Slide />} />
       </Routes>
     </div>
   );
